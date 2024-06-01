3be5652d32b9b5e5e06a7bc9dbf2a18b0d3622c9
lucaZH <lucarandria1@gmail.com>
2024-06-01 16:14:22 +0300

    Update Hero component styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,23 @@
 import { FC } from "react";
 import Social from "./ui/Social";
-import Pagination from "./ui/Pagination";
 import CusttomText from "./ui/CustomText";
 const Hero: FC = () => {
   return (
     <>
-      <div className="flex flex-row justify-between w-full h-full gap-60 text-white absolute mt-16 px-16">
+      <div className="flex flex-row justify-between w-full h-full gap-60 text-white absolute mt-[10%] px-16">
         <Social />
-        <div>
+        <div className="ml-[20%]">
           <CusttomText text="A HIKING GUIDE" />
-          <h1 className="text-6xl ">
+          <h1 className="text-6xl w-9/12 font-serif">
             Be Prepared Tor The Mountains And Beyond
           </h1>
         </div>
-        <Pagination />
       </div>
-      <img src="/img/BG.png" alt="" className="-z-40 mt-52" />
+      <img
+        src="/img/BG.png"
+        alt=""
+        className="-z-40 mt-52 object-cover w-screen"
+      />
     </>
   );
 };
