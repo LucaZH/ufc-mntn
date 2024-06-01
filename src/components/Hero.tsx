ffcf7792bcfb42b475ddd010bea4ecd496cea93c
lucaZH <lucarandria1@gmail.com>
2024-06-01 15:25:11 +0300

    Refactor Hero component to include Social, Pagination, and CustomText components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,21 @@
 import { FC } from "react";
-
+import Social from "./ui/Social";
+import Pagination from "./ui/Pagination";
+import CusttomText from "./ui/CustomText";
 const Hero: FC = () => {
   return (
     <>
-      <div className="flex flex-row justify-between w-full h-full gap-60 text-white absolute mt-16">
-        <div className="">social</div>
-        <div className="">
-          <span className="text-orange-400">A Hiking guide</span>
+      <div className="flex flex-row justify-between w-full h-full gap-60 text-white absolute mt-16 px-16">
+        <Social />
+        <div>
+          <CusttomText text="A HIKING GUIDE" />
           <h1 className="text-6xl ">
             Be Prepared Tor The Mountains And Beyond
           </h1>
         </div>
-        <div className="">pagination</div>
+        <Pagination />
       </div>
-      <img src="/img/BG.png" alt="" className="-z-40 mt-32" />
+      <img src="/img/BG.png" alt="" className="-z-40 mt-52" />
     </>
   );
 };
