ee67962ccc74a9c0b5a9dbbdda4469986b60b37c
lucaZH <lucarandria1@gmail.com>
2024-06-01 15:30:27 +0300

    Add CustomText component

---
+++ src/components/ui/CustomText.tsx
@@ -0,0 +1,17 @@
+function CustomText({ text }: { text: string }) {
+  return (
+    <div className="flex text-[#FBD784] items-center gap-4">
+      <svg
+        width="72"
+        height="2"
+        viewBox="0 0 72 2"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="72" height="2" fill="#FBD784" />
+      </svg>
+      <span className="font-semibold">{text}</span>
+    </div>
+  );
+}
+export default CustomText;
