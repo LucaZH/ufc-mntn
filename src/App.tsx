71d1b38b008d342d38456601fda198e4bd902c8a
lucaZH <lucarandria1@gmail.com>
2024-06-01 15:42:26 +0300

    Add components and styling to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
-import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pagination from "./components/ui/Pagination";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Pagination />
+      <div>
+        <img src="/img/HG.png" className="w-full absolute -z-50 cloud" />
+        <svg
+          width="100%"
+          height="1200"
+          viewBox="0 0 100% 1200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute -z-40"
+        >
+          <rect width="100%" height="1200" fill="url(#paint0_linear_11_94)" />
+          <defs>
+            <linearGradient
+              id="paint0_linear_11_94"
+              x1="1015.5"
+              y1="937.5"
+              x2="550"
+              y2="-365"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#0B1D26" stop-opacity="0" />
+              <stop offset="1" stop-color="#0B1D26" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <Navbar />
+        <Hero />
+      </div>
+    </>
+  );
 }
 export default App;
