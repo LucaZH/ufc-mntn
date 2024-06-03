951ed7334d81edbc5d1f2a88779ca263c5d26e95
lucaZH <lucarandria1@gmail.com>
2024-06-03 08:19:41 +0300

    Update Num component styling

--- src/components/ui/Num.tsx
+++ src/components/ui/Num.tsx
@@ -1,4 +1,8 @@
 function Num({ n }: { n: string }) {
-  return <h1 className="text-9xl text-white opacity-65 absolute">{n}</h1>;
+  return (
+    <h1 className="text-9xl text-white opacity-15 font-black absolute -top-16">
+      {n}
+    </h1>
+  );
 }
 export default Num;
