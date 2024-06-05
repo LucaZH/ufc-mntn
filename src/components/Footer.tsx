87e4f80c73354eb2b99299554264423cabcf5844
lucaZH <lucarandria1@gmail.com>
2024-06-05 14:36:38 +0300

    Add footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,71 @@
+function Footer() {
+  return (
+    <footer className="text-white pb-10 px-14 md:px-36">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between">
+        <div className="mb-6 md:mb-0">
+          <h2 className="text-2xl font-bold">MNTN</h2>
+          <p className="mt-4">
+            Get out there & discover your next slope, mountain & destination!
+          </p>
+          <p className="mt-4 text-gray-400">
+            &copy; 2023 MNTN, Inc.{" "}
+            <a href="#" className="text-white">
+              Terms & Privacy
+            </a>
+          </p>
+        </div>
+        <div className="mb-6 md:mb-0">
+          <h3 className="font-bold text-yellow-400">More on The Blog</h3>
+          <ul className="mt-4 space-y-2">
+            <li>
+              <a href="#" className="text-white hover:underline">
+                About MNTN
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Contributors & Writers
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Write For Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Privacy Policy
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="font-bold text-yellow-400">More on MNTN</h3>
+          <ul className="mt-4 space-y-2">
+            <li>
+              <a href="#" className="text-white hover:underline">
+                The Team
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Jobs
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-white hover:underline">
+                Press
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
