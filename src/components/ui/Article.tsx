0e05f9466e91577a9ea7034ad116b414d646cb70
lucaZH <lucarandria1@gmail.com>
2024-06-05 14:02:30 +0300

    Add icon for Read more

--- src/components/ui/Article.tsx
+++ src/components/ui/Article.tsx
@@ -32,9 +32,23 @@ function Arcticle({
           <p className="mt-4">{desc}</p>
           <a
             href={link}
-            className="text-[#FBD784] font-semibold font-mono text-sm mt-4 block"
+            className="text-[#FBD784] font-semibold font-mono text-sm mt-4 flex items-center gap-2 w-fit"
           >
             Read more
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-4"
+            >
+              <path
+                d="M5 12H19M19 12L13 6M19 12L13 18"
+                stroke="#FBD784"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </a>
         </div>
       </div>
